d6b946d61b51aa1e4a62f5edcbc99266be2d23ba
Tomoki Morita <tmsongbooks215@gmail.com>
2020-01-19 19:29:47 +0900

    Add ontouchstart attribute for pagination link

--- components/PaginationMinimal.vue
+++ components/PaginationMinimal.vue
@@ -2,25 +2,25 @@
   <nav v-if="hasPage()" aria-label="Page navigation">
     <ul class="pagination">
       <li class="first page-item w-25" :class="disabled('first')">
-        <n-link :to="pageLink(1)" class="page-link text-center" append exact>
+        <n-link :to="pageLink(1)" class="page-link text-center" append exact ontouchstart="">
           <fa icon="angle-double-left" />
         </n-link>
       </li>
 
       <li class="prev page-item w-25" :class="disabled('prev')">
-        <n-link :to="pageLink(pagination.currentPage - 1)" class="page-link text-center" append exact>
+        <n-link :to="pageLink(pagination.currentPage - 1)" class="page-link text-center" append exact ontouchstart="">
           <fa icon="angle-left" />
         </n-link>
       </li>
 
       <li class="next page-item w-25" :class="disabled('next')">
-        <n-link :to="pageLink(pagination.currentPage + 1)" class="page-link text-center" exact>
+        <n-link :to="pageLink(pagination.currentPage + 1)" class="page-link text-center" exact ontouchstart="">
           <fa icon="angle-right" />
         </n-link>
       </li>
 
       <li class="last page-item w-25" :class="disabled('last')">
-        <n-link :to="pageLink(pagination.pageCount)" class="page-link text-center" exact>
+        <n-link :to="pageLink(pagination.pageCount)" class="page-link text-center" exact ontouchstart="">
           <fa icon="angle-double-right" />
         </n-link>
       </li>
