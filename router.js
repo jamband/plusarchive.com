b6f9fafcb88148be54155f605136a7e41603ffb4
Tomoki Morita <tmsongbooks215@gmail.com>
2020-03-06 23:39:23 +0900

    Use code splitting in router.js

--- router.js
+++ router.js
@@ -1,39 +1,77 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from '~/pages/index'
-import Tracks from '~/pages/tracks'
-import Track from '~/pages/track'
-import Playlists from '~/pages/playlists'
-import Playlist from '~/pages/playlist'
-import Labels from '~/pages/labels'
-import Stores from '~/pages/stores'
-import Bookmarks from '~/pages/bookmarks'
-import About from '~/pages/about'
-import Contact from '~/pages/contact'
-import Privacy from '~/pages/privacy'
-import ThirdPartyLicenses from '~/pages/third-party-licenses'
-
 Vue.use(Router)
+
+function interopDefault (promise) {
+  return promise.then(m => m.default || m)
+}
 
 export function createRouter () {
   return new Router({
     mode: 'history',
     linkActiveClass: '',
     linkExactActiveClass: '',
-    routes: [
-      { name: 'home', path: '/', component: Home },
-      { name: 'tracks', path: '/tracks', component: Tracks },
-      { name: 'track', path: '/track/:id([\\w-]{11})', component: Track },
-      { name: 'playlists', path: '/playlists', component: Playlists },
-      { name: 'playlist', path: '/playlist/:id([\\w-]{11})', component: Playlist },
-      { name: 'labels', path: '/labels', component: Labels },
-      { name: 'stores', path: '/stores', component: Stores },
-      { name: 'bookmarks', path: '/bookmarks', component: Bookmarks },
-      { name: 'about', path: '/about', component: About },
-      { name: 'contact', path: '/contact', component: Contact },
-      { name: 'privacy', path: '/privacy', component: Privacy },
-      { name: 'third-party-licenses', path: '/third-party-licenses', component: ThirdPartyLicenses }
-    ]
+    routes: [{
+      name: 'home',
+      path: '/',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/index' /* webpackChunkName: "pages/index" */))
+    }, {
+      name: 'tracks',
+      path: '/tracks',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/tracks' /* webpackChunkName: "pages/tracks" */))
+    }, {
+      name: 'track',
+      path: '/track/:id([\\w-]{11})',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/track' /* webpackChunkName: "pages/track" */))
+    }, {
+      name: 'playlists',
+      path: '/playlists',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/playlists' /* webpackChunkName: "pages/playlists" */))
+    }, {
+      name: 'playlist',
+      path: '/playlist/:id([\\w-]{11})',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/playlist' /* webpackChunkName: "pages/playlist" */))
+    }, {
+      name: 'labels',
+      path: '/labels',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/labels' /* webpackChunkName: "pages/labels" */))
+    }, {
+      name: 'stores',
+      path: '/stores',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/stores' /* webpackChunkName: "pages/stores" */))
+    }, {
+      name: 'bookmarks',
+      path: '/bookmarks',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/bookmarks' /* webpackChunkName: "pages/bookmarks" */))
+    }, {
+      name: 'about',
+      path: '/about',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/about' /* webpackChunkName: "pages/about" */))
+    }, {
+      name: 'contact',
+      path: '/contact',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/contact' /* webpackChunkName: "pages/contact" */))
+    }, {
+      name: 'privacy',
+      path: '/privacy',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/privacy' /* webpackChunkName: "pages/privacy" */))
+    }, {
+      name: 'third-party-licenses',
+      path: '/third-party-licenses',
+      pathToRegexpOptions: { strict: true },
+      component: () => interopDefault(import('~/pages/third-party-licenses' /* webpackChunkName: "pages/third-party-licenses" */))
+    }]
   })
 }
