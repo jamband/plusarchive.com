10c85c86f3f8c21b02fb007725fee8428295e38d
Tomoki Morita <tmsongbooks215@gmail.com>
2020-03-15 15:07:17 +0900

    Improve loading animation for player

--- components/ThePlayerLoading.vue
+++ components/ThePlayerLoading.vue
@@ -1,6 +1,6 @@
 <template functional>
   <div class="player-loading">
-    ...
+    <span>.</span><span>.</span><span>.</span>
   </div>
 </template>
 
@@ -18,9 +18,14 @@
   left: 50%;
   transform: translate(-50%, -50%);
   pointer-events: none;
-  animation: blink .7s ease infinite;
   font-size: 150%;
   font-weight: $font-weight-bold;
   color: $secondary;
+
+  > span {
+    &:nth-child(1) { animation: .8s ease .3s infinite blink; }
+    &:nth-child(2) { animation: .8s ease .4s infinite blink; }
+    &:nth-child(3) { animation: .8s ease .5s infinite blink; }
+  }
 }
 </style>
