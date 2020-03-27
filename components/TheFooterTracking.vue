f4d9fc5a06eb965e5c117256a9822f33f45435e4
Tomoki Morita <tmsongbooks215@gmail.com>
2020-03-27 17:54:17 +0900

    Minor fix for TheFooterTracking component

--- components/TheFooterTracking.vue
+++ components/TheFooterTracking.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <fa icon="info-circle" />
-    This site uses Google Analytics to provide better experience.
-    By pressing <button class="btn btn-link align-baseline m-0 p-0" @click="optIn()">I ACCEPT</button>, you consent.
-    See <NLink :to="{ name: 'privacy' }">Privacy Policy</NLink> for details.
+    <fa icon="info-circle" fixed-width />
+    This site uses Google Analytics to provide better experience. By pressing
+    <button class="btn btn-link align-baseline m-0 p-0" @click="optIn()">
+      I ACCEPT
+    </button>
+    , you consent. See
+    <NLink :to="{ name: 'privacy' }">
+      Privacy Policy
+    </NLink>
+    for details.
   </div>
 </template>
 
