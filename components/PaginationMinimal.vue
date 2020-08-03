95cd204956b26c598c57ab5590d4633954d550bb
Tomoki Morita <tmsongbooks215@gmail.com>
2020-08-03 22:11:18 +0900

    Fix some styles for pagination component

--- components/PaginationMinimal.vue
+++ components/PaginationMinimal.vue
@@ -1,23 +1,23 @@
 <template>
   <nav v-if="hasPage()" aria-label="Page navigation">
-    <ul class="pagination">
+    <ul class="pagination text-center">
       <li class="page-item w-25" :class="disabled('first')">
-        <NLink :to="pageLink(1)" class="page-link text-center" append exact>
+        <NLink :to="pageLink(1)" class="page-link" append exact>
           <fa icon="angle-double-left" />
         </NLink>
       </li>
       <li class="page-item w-25" :class="disabled('prev')">
-        <NLink :to="pageLink(currentPage - 1)" class="page-link text-center" append exact>
+        <NLink :to="pageLink(currentPage - 1)" class="page-link" append exact>
           <fa icon="angle-left" />
         </NLink>
       </li>
       <li class="page-item w-25" :class="disabled('next')">
-        <NLink :to="pageLink(currentPage + 1)" class="page-link text-center" exact>
+        <NLink :to="pageLink(currentPage + 1)" class="page-link" exact>
           <fa icon="angle-right" />
         </NLink>
       </li>
       <li class="page-item w-25" :class="disabled('last')">
-        <NLink :to="pageLink(pageCount)" class="page-link text-center" exact>
+        <NLink :to="pageLink(pageCount)" class="page-link" exact>
           <fa icon="angle-double-right" />
         </NLink>
       </li>
