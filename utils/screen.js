b8d3672d1f33a1a520fc9bfbede0f35c22a9a114
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-28 01:14:11 +0900

    Remove unnecessary lines

--- utils/screen.js
+++ utils/screen.js
@@ -2,9 +2,6 @@ export const hasTouchScreen = () => {
   if (navigator.maxTouchPoints > 0) {
     return true
   }
-  if (navigator.msMaxTouchPoints > 0) {
-    return true
-  }
   if (window.matchMedia('(pointer:coarse)').matches) {
     return true
   }
