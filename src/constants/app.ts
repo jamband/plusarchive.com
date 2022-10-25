7e806dcc6c6b0ec048e03e0d86a1b82f7fc342f7
Tomoki Morita <tmsongbooks215@gmail.com>
2022-10-25 20:43:33 +0900

    fix the APP_REPOSITORY_NAME constant value

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -3,6 +3,6 @@ export const APP_DESCRIPTION =
   `${APP_NAME} is music archive website for everyday` as const;
 export const APP_PRIMARY_COLOR = "c45c65" as const;
 export const APP_URL = "https://plusarchive.com" as const;
-export const APP_REPOSITORY_NAME = "jamband/api.plusarchive.com" as const;
+export const APP_REPOSITORY_NAME = "jamband/plusarchive.com" as const;
 export const APP_REPOSITORY_URL =
   `https://github.com/${APP_REPOSITORY_NAME}` as const;
