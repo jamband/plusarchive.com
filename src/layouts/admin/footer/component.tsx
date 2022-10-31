36a0612af6e21de708fc3cbfb3d267f09ffa7bd9
Tomoki Morita <tmsongbooks215@gmail.com>
2022-10-31 21:17:50 +0900

    improve footer component styles

--- src/layouts/admin/footer/component.tsx
+++ src/layouts/admin/footer/component.tsx
@@ -2,7 +2,7 @@ import { APP_NAME } from "~/constants/app";
 import { IconLock } from "~/icons/lock";
 
 export const Component: React.FC = () => (
-  <footer className="fixed bottom-0 z-20 w-full bg-gray-700 text-rose-500 shadow-[0_-1px_2px] shadow-gray-900">
+  <footer className="fixed bottom-0 z-20 w-full bg-gray-700 text-sm text-rose-500 shadow-[0_-1px_2px] shadow-gray-900 md:text-base">
     <div className="container mx-auto py-4 text-center font-semibold">
       <IconLock className="mr-1.5 h-4 w-4 align-[-0.125em]" />
       {APP_NAME}
--- src/layouts/footer/component.tsx
+++ src/layouts/footer/component.tsx
@@ -4,7 +4,7 @@ import { APP_NAME } from "~/constants/app";
 import type { _Props } from "./types";
 
 export const Component: React.FC<_Props> = (props) => (
-  <footer className="fixed bottom-0 z-20 w-full bg-gray-700 shadow-[0_-1px_2px] shadow-gray-900">
+  <footer className="fixed bottom-0 z-20 w-full bg-gray-700 text-sm shadow-[0_-1px_2px] shadow-gray-900 md:text-base">
     {props.player.id !== "" && !props.isPlayerVisible ? (
       <div className="my-2 flex items-center justify-center lg:container lg:mx-auto">
         <Link href={`/${props.player.type}s/${props.player.id}`}>
@@ -14,7 +14,7 @@ export const Component: React.FC<_Props> = (props) => (
         </Link>
         <CloseButton
           className="mr-2 rounded px-2 py-1 hover:bg-gray-600 hover:text-gray-100 focus:bg-gray-600 focus:text-rose-500"
-          iconClass="h-5 w-5 align-[-0.25em]"
+          iconClass="h-5 w-5 align-[-0.4em] md:align-[-0.25em]"
           onClick={props.resetPlayer}
         />
       </div>
