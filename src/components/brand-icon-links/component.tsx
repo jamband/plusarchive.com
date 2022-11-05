3d03d319386233604aa04add85e0bf414afbc8de
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-05 11:25:51 +0900

    improve some styles for brand-icon-links

--- src/components/brand-icon-links/component.tsx
+++ src/components/brand-icon-links/component.tsx
@@ -2,7 +2,7 @@ import { ExternalLink } from "../external-link";
 import type { _Props } from "./types";
 
 export const Component: React.FC<_Props> = (props) => (
-  <ul className="flex gap-x-3">
+  <ul className="flex flex-wrap gap-x-3 gap-y-1">
     {props.links.map((link, index) => (
       <li key={index}>
         <ExternalLink key={index} href={link} className="hover:text-gray-100">
