fac83e98718526143d416435b66412722f42a9a4
Tomoki Morita <tmsongbooks215@gmail.com>
2024-01-28 01:43:21 +0900

    add some tests for tag links component

--- src/components/tag-links/index.test.tsx
+++ src/components/tag-links/index.test.tsx
@@ -5,7 +5,13 @@ test("", () => {
   render(<TagLinks data={["foo", "bar", "baz"]} pathname="/somewhere" />);
 
   const links = screen.getAllByRole("link");
+
   expect(links[0]).toHaveAttribute("href", "/somewhere?tag=foo");
+  expect(links[0]).toHaveTextContent("foo");
+
   expect(links[1]).toHaveAttribute("href", "/somewhere?tag=bar");
+  expect(links[1]).toHaveTextContent("bar");
+
   expect(links[2]).toHaveAttribute("href", "/somewhere?tag=baz");
+  expect(links[2]).toHaveTextContent("baz");
 });
