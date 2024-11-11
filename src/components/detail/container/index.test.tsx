beaa328082d4f434af3bd0c518025aa363a72c05
Tomoki Morita <tmsongbooks215@gmail.com>
2024-11-11 15:56:12 +0900

    minor fixes for details component test

--- src/components/detail/container/index.test.tsx
+++ src/components/detail/container/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import { DetailContainer } from ".";
 
 test("", () => {
-  render(<DetailContainer>foo</DetailContainer>);
+  render(<DetailContainer className="">foo</DetailContainer>);
 
   const text = screen.getByText("foo");
   expect(text).toBeInTheDocument();
