be02a7eda5c90360a019be390d01d2ad88365c04
Tomoki Morita <tmsongbooks215@gmail.com>
2025-01-11 18:32:55 +0900

    add next-env.d.ts to .gitignore

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -3,4 +3,5 @@ import { rm } from "node:fs/promises";
 await rm(".next", { recursive: true, force: true });
 await rm(".swc", { recursive: true, force: true });
 await rm("node_modules", { recursive: true, force: true });
+await rm("next-env.d.ts", { recursive: true, force: true });
 await rm("tsconfig.tsbuildinfo", { recursive: true, force: true });
