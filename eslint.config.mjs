363f7c72f0ae1e690d5e7170a1a584279154319c
Tomoki Morita <tmsongbooks215@gmail.com>
2025-10-29 14:24:46 +0900

    update next version to 16
    and some improvements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,41 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
+import next from "eslint-config-next/core-web-vitals";
+import ts from "eslint-config-next/typescript";
+import jestDom from "eslint-plugin-jest-dom";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import testingLibrary from "eslint-plugin-testing-library";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-});
-
-const eslintConfig = [
+const eslintConfig = defineConfig(
+  [globalIgnores([".next/", "next-env.d.ts"])],
   {
-    ignores: [".next/**", "next-env.d.ts"],
+    files: ["**/*.{js,mjs}"],
+    extends: [js.configs.recommended],
   },
-  ...compat.config({
-    extends: [
-      "eslint:recommended",
-      "plugin:jest-dom/recommended",
-      "plugin:jsx-a11y/recommended",
-      "plugin:testing-library/react",
-      "next/core-web-vitals",
-      "next/typescript",
-    ],
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [ts],
     rules: {
       "@typescript-eslint/consistent-type-imports": "error",
     },
-  }),
-];
+  },
+  {
+    files: ["src/**/*.tsx"],
+    rules: {
+      ...jsxA11y.flatConfigs.recommended.rules,
+    },
+  },
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    extends: [next],
+  },
+  {
+    files: ["src/**/*.test.{ts,tsx}"],
+    extends: [
+      jestDom.configs["flat/recommended"],
+      testingLibrary.configs["flat/react"],
+    ],
+  },
+);
 
 export default eslintConfig;
