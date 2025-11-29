b99b33511dae692e8878fb768201b17579a97548
Tomoki Morita <tmsongbooks215@gmail.com>
2025-11-29 22:42:44 +0900

    use vitejs/plugin-react instead of vitejs/plugin-react-swc
    because there is a version inconsistency in swc/helpers that next and vitejs/plugin-react-swc depend on

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
